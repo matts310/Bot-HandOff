9ffe2ecdff1c8aa820a5727db9b5b868b85e94d9
Matt Schwartz <mattschwartz310@gmail.com>
2017-08-03 19:11:36 -0700

    cleaned up dependency import order

--- src/applyHandoffMiddleware.ts
+++ src/applyHandoffMiddleware.ts
@@ -3,14 +3,12 @@ import { IAddress, IMessage, Message, Session, UniversalBot } from 'botbuilder';
 import { ConversationState, MessageType } from './constants';
 import { IConversation } from './IConversation';
 import { IHandoffEventMessage, isIHandoffEventMessage } from './IHandoffMessage';
-import { InMemoryProvider } from './provider/InMemoryProvider';
-// import { IHandoffEventMessage, isIHandoffEventMessage } from './IHandoffEventMessage';
-
 import { getAddAddressesForHandoffMessageMiddleware } from './middleware/getAddAddressesForHandoffMessageMiddleware';
 import { getHandoffMessageEventInterceptor } from './middleware/getHandoffMessageEventInterceptor';
 import { getRouteMessgeMiddleware } from './middleware/getRouteMessageMiddleware';
 import { getTranscribeBotMessagesMiddleware } from './middleware/getTranscribeBotMessagesMiddleware';
 import { getTranscribeNonBotMessagesMiddleware } from './middleware/getTranscribeNonBotMessagesMiddleware';
+import { InMemoryProvider } from './provider/InMemoryProvider';
 import { IProvider } from './provider/IProvider';
 import { routeAgentMessage } from './routers/routeAgentMessage';
 import { routeCustomerMessage } from './routers/routeCustomerMessage';
